d234de9665f223579550c213fdd7edfb34381986
Isaac <isaac.hdz.perez@gmail.com>
2024-09-24 10:16:03 -0600

    Initial Commit

---
+++ Tarea4Movil/Tarea4MovilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tarea4MovilApp.swift
+//  Tarea4Movil
+//
+//  Created by Alumno on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Tarea4MovilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tarea4MovilTests/Tarea4MovilTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tarea4MovilTests.swift
+//  Tarea4MovilTests
+//
+//  Created by Alumno on 24/09/24.
+//
+
+import Testing
+@testable import Tarea4Movil
+
+struct Tarea4MovilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
