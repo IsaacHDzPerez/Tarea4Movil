963098eab060c6d285684fbb182cd96dfcc5e614
Isaac <isaac.hdz.perez@gmail.com>
2024-10-10 17:17:40 -0600

    initial commit

---
+++ Tarea4Movil/ChartListView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartListView.swift
+//  Tarea4Movil
+//
+//  Created by Alumno on 27/09/24.
+//
+
+import SwiftUI
+
+struct ChartListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartListView()
+}
